e073ba43946e16bb9192569420f5feeea349be78
Paul WB <higgsbison@gmail.com>
2017-01-15 18:18:20 -0800

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
@@ -9,7 +9,12 @@ import reducers from './reducers/';
 import './styles.css';
 
 const App = () => {
-  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducers, {}, composeEnhancers(
+    applyMiddleware(ReduxThunk),
+  ));
+  /* eslint-enable */
   return (
     <Provider store={store}>
       <Routes />
