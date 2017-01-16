15ada985d88a74fe46d0f13357656347b074df57
Paul WB <higgsbison@gmail.com>
2017-01-15 16:15:40 -0800

    add webpack-dev-server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div> Fig Knight React Starter </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
