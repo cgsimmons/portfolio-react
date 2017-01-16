1676c7994b081ae990366c7cbc72a8a5112edd50
Paul WB <higgsbison@gmail.com>
2017-01-15 18:14:20 -0800

    add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
 import Routes from './router';
+import reducers from './reducers/';
 import './styles.css';
 
 const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
   return (
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   );
 };
 
