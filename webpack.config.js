501668fb9a274b7c3c9010fcc10e204e69a7b6ee
Paul WB <higgsbison@gmail.com>
2017-01-15 17:30:56 -0800

    add style and img loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,40 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: 'build/',
   },
   module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/,
-        exclude: '/node_modules/',
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader',
+        }),
+        test: /\.css$/,
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 },
+          },
+          'image-webpack-loader',
+        ],
       },
     ],
   },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ],
 };
 
 module.exports = config;
