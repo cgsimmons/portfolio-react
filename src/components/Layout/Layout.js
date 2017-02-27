b40574c67b8e96aec021be7d9389a931f6dc46cd
cgsimmons <chris.simmons.alt@gmail.com>
2017-02-26 16:32:51 -0800

    Add shifting content

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import NavBar from '../NavBar/NavBar';
-import './style.css';
+import './style.scss';
 
-export default (props) => {
-  return (
+class Layout extends React.Component {
+  render() {
+    return (
 
-    <div className="app-container">
-      <header />
-      <div className="app-body">
-        <NavBar />
-        <div className="app-content">
-          {props.children}
+      <div className="app-container">
+        <header />
+        <div className="app-body">
+          <NavBar />
+          <div className={`app-content${this.props.isNavBarHovering ? ' shift-content' : ''}`}>
+            {this.props.children}
+          </div>
         </div>
+        <footer />
       </div>
-      <footer />
-    </div>
-  );
-};
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isNavBarHovering: state.navBar.hovering,
+  }
+}
+
+export default connect(mapStateToProps, null)(Layout);
