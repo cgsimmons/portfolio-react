626d57308ddab6c61cb31d50dae354bc6d74a860
cgsimmons <chris.simmons.alt@gmail.com>
2017-03-04 14:10:44 -0800

    Change colors

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,11 @@ import './Banner.scss';
 export default () => {
   return (
     <div className="Banner" >
-      <h1>Chris Simmons!</h1>
+      <h1>
+        <span className="overlay">christopher</span>
+        <span className="firstName">christopher</span>
+        <span className="lastName">SIMMONS</span>
+      </h1>
     </div>
   );
 };
