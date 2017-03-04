643ec5abf79aefa025cb83e900ea5e14636f42ec
cgsimmons <chris.simmons.alt@gmail.com>
2017-03-03 19:21:29 -0800

    Add bg to icons

--- src/components/sections/Interests/Interests.js
+++ src/components/sections/Interests/Interests.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => {
   return (
-    <div id="Work" className="section">Work History</div>
+    <div id="Interests" className="section">Interests</div>
   );
 };
