583b3bb0b3b00007f94ab2fd73d769a13a6e23b2
cgsimmons <chris.simmons.alt@gmail.com>
2017-03-03 23:33:22 -0800

    Add dim to nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NavBar from '../navigation/NavBar/NavBar';
 import NavHam from '../navigation/NavHam/NavHam';
+import { setNavBarHover } from '../../actions/NavBarActions';
 import './style.scss';
 
 class Layout extends React.Component {
+  handleClick = () => {
+    this.props.setHover(false);
+  }
   render() {
     return (
 
@@ -14,7 +18,10 @@ class Layout extends React.Component {
         </header>
         <div className="app-body">
           <NavBar />
-          <div className={`app-content${this.props.isNavBarHovering ? ' shift-content' : ''}`}>
+          <div
+            className={`app-content${this.props.isNavBarHovering ? ' shift-content' : ''}`}
+            onClick={this.handleClick}
+          >
             {this.props.children}
           </div>
         </div>
@@ -30,4 +37,12 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Layout);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setHover: (hover) => {
+      dispatch(setNavBarHover(hover));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
--- src/components/navigation/NavBar/NavLinks/NavLinks.js
+++ src/components/navigation/NavBar/NavLinks/NavLinks.js
@@ -16,53 +16,56 @@ class NavLinks extends React.Component {
   handleClick = () => {
     setTimeout(() => { this.props.setHover(false); }, 650);
   }
+  handleHoverTrue = () => {
+    this.props.setHover(true);
+  }
   render() {
     return (
       <div className="NavLinks">
         <ul>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#root" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <MdHome />
               <span>Top of Page</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#About" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <FaInfoCircle />
               <span>About Me</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#Skills" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <FaCode />
               <span>Skills</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#Portfolio" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <MdWork />
               <span>Portfolio</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#Education" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <FaGraduationCap />
               <span>Education</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#Work" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <FaBlackTie />
               <span>Work</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#Volunteer" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <FaHeart />
               <span>Volunteer</span>
             </Scrollchor>
           </li>
-          <li>
+          <li onMouseEnter={this.handleHoverTrue}>
             <Scrollchor to="#Interests" animate={{ offset: -45, duration: 800 }} beforeAnimate={this.handleClick}>
               <FaThumbsOUp />
               <span>Interests</span>
