c7ae28930a299abf909314748febcff7efb6f8a7
cgsimmons <chris.simmons.alt@gmail.com>
2017-03-24 20:59:17 -0700

    Setup general la layout

--- src/components/sections/Portfolio/Portfolio.js
+++ src/components/sections/Portfolio/Portfolio.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import pokemanGo from '../../../assets/images/pokeman-go.png';
 import rails from '../../../assets/icons/rails.svg';
+import pokemanGo from '../../../assets/gifs/pokeman-go-sm.gif';
+import findPet from '../../../assets/gifs/find-pet-sm.gif';
+import twitterScout from '../../../assets/gifs/twitter-scout-sm.gif';
+import snippet from '../../../assets/gifs/snippet-sm.gif';
+import awesome from '../../../assets/gifs/awesome-sm.gif';
+
 import './Portfolio.scss';
 
 export default () => {
@@ -14,7 +19,7 @@ export default () => {
         <div className="img-container">
           <img src={pokemanGo} alt="Pokeman Go logo" />
         </div>
-        <div className="top-container">
+        <div className="middle-container">
           <div className="title-container">
             <h3 className="title">Pokeman GO</h3>
           </div>
@@ -22,40 +27,97 @@ export default () => {
             <img className="icon" alt="Rails" src={rails} />
           </div>
         </div>
-        <div className="content">
-          <p>details </p>
+        <div className="bottom-container">
+          <p>The winning project of the weekend-long Comedy Hack Day
+             Vancouver Hack-a-thon. This Pokemon parody web app allows
+              users to take pictures of their friends and use real facial
+               recognition software to determine which type of Pokeman
+                they caught.</p>
         </div>
       </div>
       <div className="inner-container">
         <div className="img-container">
-          <img src={pokemanGo} alt="Pokeman Go logo" />
+          <img src={twitterScout} alt="Twitter Scout" />
         </div>
-        <div className="top-container">
+        <div className="middle-container">
           <div className="title-container">
-            <h3 className="title">Pokeman GO</h3>
+            <h3 className="title">Twitter Scout</h3>
           </div>
           <div className="icons-container">
             <img className="icon" alt="Rails" src={rails} />
           </div>
         </div>
-        <div className="content">
-          <p>details </p>
+        <div className="bottom-container">
+          <p>A Twitter manager using universal routing /
+           Javascript. Pages are generated and rendered
+            dynamically on the server using the same code
+             as the client. The client loads server rendered
+              markup while the bundle is downloading. It
+               also allows search engines to crawl the server
+                rendered pages.
+          </p>
         </div>
       </div>
       <div className="inner-container">
         <div className="img-container">
-          <img src={pokemanGo} alt="Pokeman Go logo" />
+          <img src={findPet} alt="Find My Pet" />
         </div>
-        <div className="top-container">
+        <div className="middle-container">
           <div className="title-container">
-            <h3 className="title">Pokeman GO</h3>
+            <h3 className="title">Find My Pet</h3>
           </div>
           <div className="icons-container">
             <img className="icon" alt="Rails" src={rails} />
           </div>
         </div>
-        <div className="content">
-          <p>details </p>
+        <div className="bottom-container">
+          <p>A web app designed to help people report lost and
+             found pets. The app uses an AJAX front end with
+              Ruby on Rails for the back end. The project was
+               created over the course of a weekend with a team
+                of 8 members in conjuction with Envisioning
+                 Lab&apos;s Test Tube program.
+          </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="img-container">
+          <img src={snippet} alt="Code Snippets" />
+        </div>
+        <div className="middle-container">
+          <div className="title-container">
+            <h3 className="title">Code Snippets</h3>
+          </div>
+          <div className="icons-container">
+            <img className="icon" alt="Rails" src={rails} />
+          </div>
+        </div>
+        <div className="bottom-container">
+          <p>A Rails app that allows users to save snippets
+             of code from 23 different languages. The app
+            uses an embedded javascript syntax highlighting
+           editor ( <a target="_blank" rel="noreferrer noopener" href="https://ace.c9.io">Ace</a> ). </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="img-container">
+          <img src={awesome} alt="Awesome Answers" />
+        </div>
+        <div className="middle-container">
+          <div className="title-container">
+            <h3 className="title">Awesome Answers</h3>
+          </div>
+          <div className="icons-container">
+            <img className="icon" alt="Rails" src={rails} />
+          </div>
+        </div>
+        <div className="bottom-container">
+          <p>This is a simple Question & Answer web app built on
+             Rails. An API is implemented as well. Some of the
+              more interesting gems used include:
+               omniauth-twitter, fog, carrierwave,
+                active_model_serializers, cancancan and
+                 simple_form.</p>
         </div>
       </div>
     </div>
