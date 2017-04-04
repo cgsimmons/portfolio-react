4ea14a195fa05efdd8210252d9e3f522de67af14
cgsimmons <chris.simmons.alt@gmail.com>
2017-04-03 17:15:48 -0700

    Tweak some stuff

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Layout from './components/Layout/Layout';
 import IndexPage from './components/IndexPage/IndexPage';
 
 
 export default () => {
   return (
-    <Router history={hashHistory} >
+    <Router history={browserHistory} >
       <Route path="/" component={Layout}>
         <IndexRoute component={IndexPage} />
       </Route>
