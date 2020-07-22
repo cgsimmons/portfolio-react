0e55dee7cc1301098d63c336ce52a5c990dafd4a
Chris Simmons <csimmons@adgorithmics.com>
2020-07-22 17:21:20 +0000

    Update about info

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -10,18 +10,18 @@ export default () => (
     </div>
     <div className="content">
       Experienced in Full-Stack web development, Kubernetes cluster management
-      and just being an all around solid dude. I enjoy living in rural Japan for
-      the last 9 years and I'll probably stay here a bit longer. My CS degree
-      was focused on C, C++, Java and Python but I have since moved to more web
-      development / microservices using Javascript/Typescript (NodeJS, React).
+      and just being an all around solid dude. My CS degree was focused on C,
+      C++, Java and Python but I have since moved to more web development /
+      microservices using Javascript/Typescript (NodeJS, React). I've lived in
+      Japan since 2011 and I probably won't be leaving any time soon.
       <br />
       <br />
       I enjoy working with motivated individuals and solving problems, learning
       new tools, and becoming a better software developer.
       <br />
       <br />
-      Feel free to contact me about opportunities in the San Francisco, Seattle
-      or Vancouver B.C. or Tokyo areas.
+      Feel free to contact me about opportunities in the San Francisco, Seattle,
+      Vancouver B.C. or Tokyo areas.
     </div>
   </div>
 );
